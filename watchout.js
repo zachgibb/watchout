21f5ad37dc28fb492265fd819bcb6f8814513a32
Zach Gibb <zachgibb@gmail.com>
2014-08-02 00:50:45 -0400

    Added drag functionality

--- watchout.js
+++ watchout.js
@@ -28,7 +28,7 @@ var Enemy = function () {
 var numEnemies = Math.floor(Math.random() * 18) + 6;
 var enemies = [];
 for (numEnemies; numEnemies > 0; numEnemies--) {
-  enemies.push(new Enemy);
+  enemies.push(new Enemy());
 }
 
 d3.select('svg').selectAll('circle')
@@ -42,10 +42,32 @@ d3.select('svg').selectAll('circle')
 
 // create player
 
-d3.select('svg').append('path')
-  .attr({'d':"M 250 200 L 240 215 L 260 215 z", 
-    'fill': 'black', 
-    'stroke-width': '2'});
+var Player = function(){
+  this.x = 250;
+  this.y = 200;
+};
+var player = [new Player()];
+
+var dragmove = function(d){
+  d3.select(this).attr({
+    'x': d.x = d3.event.x,
+    'y': d.y = d3.event.y
+  });
+};
+
+var dragging = d3.behavior.drag()
+  .origin(function (d) {return d;})
+  .on('drag', dragmove);
+
+d3.select('svg').append('rect').data(player)
+  .attr({
+   'x': function(d){return d.x;},
+   'y': function(d){return d.y;},
+   'width': 20,
+   'height': 20,
+   'fill': 'black'
+  }).call(dragging); 
+
 
 
 
@@ -58,3 +80,39 @@ setInterval(function(){
       'cy': function(){ return Math.random() * 380 + 10}
   });
 }, 1000);
+
+
+
+
+
+
+
+
+
+
+
+// var Player = function(){
+//   this.path = "M 250 200 L 240 215 L 260 215 z";
+//   this.x = 250;
+//   this.y = 200;
+//   this.fill = 'black';
+// };
+// var player = [new Player()];
+// d3.select('svg').append('path').data(player)
+//   .attr({
+//    'd': function (d) {return d.path;}, 
+//     'fill': function (d) {return d.fil;}, 
+//     'stroke-width': '2'
+//   }); 
+
+// var dragging = d3.behavior.drag()
+//   .origin(function (d) {return d;})
+//   .on('drag', dragmove);
+
+// function dragmove(d) {
+//   d3.select(this)
+//       .attr("cx", d.x = Math.max(radius, Math.min(width - radius, d3.event.x)))
+//       .attr("cy", d.y = Math.max(radius, Math.min(height - radius, d3.event.y)));
+// }
+
+// d3.select('path').call(dragging);
