99e42a9a3af3d9686447c7d326395ba06efc2830
Zach Gibb <zachgibb@gmail.com>
2014-08-02 13:26:34 -0400

    collision detection works while moving and still

--- watchout.js
+++ watchout.js
@@ -24,9 +24,9 @@ function getRandomColor() {
 var Enemy = function () {
   this.x = Math.random() * 480 + 10;
   this.y = Math.random() * 380 + 10;
-  this.id = Math.floor(Math.random() * 100 + 1);
+  this.id = "e" + Math.floor(Math.random() * 100 + 1);
 };
-var numEnemies = Math.floor(Math.random() * 18) + 7;
+var numEnemies = 1; //Math.floor(Math.random() * 18) + 7;
 var enemies = [];
 for (numEnemies; numEnemies > 0; numEnemies--) {
   enemies.push(new Enemy());
@@ -52,25 +52,28 @@ var Player = function(){
     var player = this;
     // indicate a hit
     d3.selectAll('circle').each(function(d){
-      
-      var xTest = (d.x > (player.x - 10) && d.x < (player.x + 23));
-      var yTest = (d.y > (player.y - 10) && d.y <(player.y + 22));
+      var enemyY = d3.select("#"+d.id).attr('cy');
+      var enemyX = d3.select("#"+d.id).attr('cx');
+      var xTest = (enemyX > (player.x - 8) && enemyX < (player.x + 22));
+      var yTest = (enemyY > (player.y - 8) && enemyY <(player.y + 22));
       if (xTest && yTest) {
-        debugger;
         console.log("HIT");
       }
     });
 
   };
 };
-var player = [new Player()];
+var playerChar = new Player();
+var player = [playerChar];
+// var player = [];
+// player.push(new Player);
 
 var dragmove = function(d){
   d3.select(this).attr({
     'x': d.x = d3.event.x,
     'y': d.y = d3.event.y
   });
-  d.hit();
+  // d.hit();
 };
 
 var dragging = d3.behavior.drag()
@@ -86,8 +89,9 @@ d3.select('svg').append('rect').data(player)
    'fill': 'black'
   }).call(dragging); 
 
-
-
+setInterval(function(){
+  playerChar.hit();
+}, 50);
 
 
 setInterval(function(){
